597bc5a6b49f44b91ba44abd13f38fea842a241f
Altair Wei <altair_wei@outlook.com>
2019-08-18 13:55:52 +0800

    IMPROVE: upgrade dialog font and style

--- src/WizUpgradeNotifyDialog.cpp
+++ src/WizUpgradeNotifyDialog.cpp
@@ -3,6 +3,7 @@
 
 #include <QString>
 #include <QWebEngineSettings>
+#include <QUrl>
 
 #include "utils/WizPathResolve.h"
 #include "share/WizWebEngineView.h"
@@ -38,22 +39,30 @@ QString WizUpgradeNotifyDialog::createMarkdownContentPage(const QString &markdow
         <head>
             <meta charset="utf-8"/>
             <title>WizNotePlus Release</title>
+            <style>
+                body {
+                    padding: 4px;
+                    font-family: -apple-system, "Noto Sans", "Helvetica Neue", Helvetica, "Nimbus Sans L", 
+                        Arial, "Liberation Sans", "PingFang SC", "Hiragino Sans GB", "Noto Sans CJK SC", 
+                        "Source Han Sans SC", "Source Han Sans CN", "Microsoft YaHei", "Wenquanyi Micro Hei", 
+                        "WenQuanYi Zen Hei", "ST Heiti", SimHei, "WenQuanYi Zen Hei Sharp", sans-serif;
+                }
+            </style>
         </head>
         <body>
             <div style="display: none;" id="content">%1</div>
-            <div id="show"></div>
             <script src="%2"></script>
             <script>
-                document.getElementById('show').innerHTML = marked(
-                    document.getElementById('content').innerText
-                );
+                const content = document.getElementById('content');
+                content.innerHTML = marked(content.innerText);
+                content.style.display = "block";
             </script>
         </body>
     </html>
     );
 
     QString libFileUrl = Utils::WizPathResolve::resourcesPath() + "files/markdown/marked-0.7.0.min.js";
-
+    
     return htmlTemplate.arg(markdownSource).arg(libFileUrl);
 }
 
@@ -62,5 +71,5 @@ void WizUpgradeNotifyDialog::showMarkdownContent(const QString &markdownSource)
     const QString html = createMarkdownContentPage(markdownSource);
     const QString tempHtmlFile = Utils::WizPathResolve::tempPath() + "release_notes.html";
     WizSaveUnicodeTextToUtf8File(tempHtmlFile, html);
-    m_web->load(tempHtmlFile);
+    m_web->load(QUrl::fromLocalFile(tempHtmlFile));
 }
