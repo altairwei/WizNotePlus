e6121851fa866909da43324f8f97cc0307586c13
Altair Wei <altair_wei@outlook.com>
2020-05-09 16:32:45 +0800

    NEW: add more information to About dialog

--- src/widgets/WizAboutDialog.cpp
+++ src/widgets/WizAboutDialog.cpp
@@ -6,6 +6,7 @@
 #include <QFileInfo>
 #include <QTextBrowser>
 #include <QVBoxLayout>
+#include <QHBoxLayout>
 #include <QIcon>
 #include <QDateTime>
 
@@ -31,17 +32,28 @@ WizAboutDialog::WizAboutDialog(QWidget *parent)
     QString strPath = QApplication::applicationDirPath();
     QFileInfo fi(strPath);
     QDateTime t = fi.lastModified();
-    QString strBuildNumber("(%1.%2.%3 %4:%5)");
+    QString strBuildNumber("<span style=\"font-size:11px\">Build Time %1.%2.%3 %4:%5</span>");
     strBuildNumber = strBuildNumber.\
             arg(t.date().year()).\
             arg(t.date().month()).\
             arg(t.date().day()).\
             arg(t.time().hour()).\
             arg(t.time().minute());
+    QLabel* labelBuildTime = new QLabel(this);
+    labelBuildTime->setText(strBuildNumber);
 
-    QString strInfo = QString(tr("<span style=\"font-size:11px\">Version %2 %3</span>")).arg(WIZ_CLIENT_VERSION, strBuildNumber);
+    QString strInfo = QString(tr("<span style=\"font-size:11px\">Version %2</span>")).arg(WIZ_CLIENT_VERSION);
     QLabel* labelBuild = new QLabel(this);
     labelBuild->setText(strInfo);
+
+    QString devInfo = QString(
+        tr("<span style=\"font-size:11px\">Development Stage %1.%2</span>").arg(WIZ_DEV_STAGE, WIZ_DEV_STAGE_VERSION));
+    QLabel* labelDev = new QLabel(this);
+    labelDev->setText(devInfo);
+
+    QString qtInfo = QString(tr("<span style=\"font-size:11px\">Build against Qt %1</span>").arg(QT_VERSION_STR));
+    QLabel* labelQt = new QLabel(this);
+    labelQt->setText(qtInfo);
 
     QTextBrowser* textCredits = new QTextBrowser(this);
     textCredits->setOpenExternalLinks(true);
@@ -62,19 +74,36 @@ WizAboutDialog::WizAboutDialog(QWidget *parent)
     QLabel* labelCopyright = new QLabel(this);
     labelCopyright->setText(tr("<span style=\"font-size:10px\">Copyright 2011-2017 Beijing Wozhi Technology Co., Ltd. All rights reserved.</span>"));
 
+    QHBoxLayout *mainLayout = new QHBoxLayout(this);
     QVBoxLayout* layout = new QVBoxLayout(this);
+
+    mainLayout->addLayout(layout);
     layout->setContentsMargins(0, 10, 0, 10);
 
     layout->addWidget(labelIcon);
-    layout->addWidget(labelProduct);
-    layout->addWidget(labelBuild);
-    layout->addWidget(textCredits);
-    layout->addWidget(labelCopyright);
     layout->setAlignment(labelIcon, Qt::AlignCenter);
+
+    layout->addWidget(labelProduct);
     layout->setAlignment(labelProduct, Qt::AlignCenter);
+
+    layout->addWidget(labelBuild);
     layout->setAlignment(labelBuild, Qt::AlignCenter);
-    layout->setAlignment(textCredits, Qt::AlignCenter);
+
+    layout->addWidget(labelDev);
+    layout->setAlignment(labelDev, Qt::AlignCenter);
+
+    layout->addWidget(labelQt);
+    layout->setAlignment(labelQt, Qt::AlignCenter);
+
+    layout->addWidget(labelBuildTime);
+    layout->setAlignment(labelBuildTime, Qt::AlignCenter);
+
+    layout->addSpacing(50);
+    
+    layout->addWidget(labelCopyright);
     layout->setAlignment(labelCopyright, Qt::AlignCenter);
+
+    mainLayout->addWidget(textCredits);
 
     setWindowTitle(tr("About WizNote"));
 }
