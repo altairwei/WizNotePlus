6fce4f7ac236895c7a3030525745c5d1af61188b
Altair Wei <altair_wei@outlook.com>
2020-05-31 10:55:50 +0800

    FIX: build against Qt 5.14.2

--- src/widgets/WizAboutDialog.cpp
+++ src/widgets/WizAboutDialog.cpp
@@ -9,6 +9,8 @@
 #include <QHBoxLayout>
 #include <QIcon>
 #include <QDateTime>
+#include <QDate>
+#include <QTime>
 
 #include <QTextStream>
 
@@ -19,60 +21,49 @@ WizAboutDialog::WizAboutDialog(QWidget *parent)
     labelIcon->setPixmap(qApp->windowIcon().pixmap(QSize(58, 58)));
 
 #if defined Q_OS_MAC
-    QString strProduct("<span style=\"font-weight:bold;font-size:14px\">WizNotePlus for Mac</span>");
+    QString strProduct("<span style='font-weight:bold;font-size:14px'>WizNotePlus for Mac</span>");
 #elif defined Q_OS_LINUX
-    QString strProduct("<span style=\"font-weight:bold;font-size:14px\">WizNotePlus for Linux</span>");
+    QString strProduct("<span style='font-weight:bold;font-size:14px'>WizNotePlus for Linux</span>");
 #else
-    QString strProduct("<span style=\"font-weight:bold;font-size:14px\">WizNotePlus for Windows</span>");
+    QString strProduct("<span style='font-weight:bold;font-size:14px'>WizNotePlus for Windows</span>");
 #endif
 
     QLabel* labelProduct = new QLabel(this);
     labelProduct->setText(strProduct);
+    labelProduct->setTextInteractionFlags(Qt::TextBrowserInteraction | Qt::TextSelectableByKeyboard);
 
-    QString strPath = QApplication::applicationDirPath();
-    QFileInfo fi(strPath);
-    QDateTime t = fi.lastModified();
-    QString strBuildNumber("<span style=\"font-size:11px\">Build Time %1.%2.%3 %4:%5</span>");
-    strBuildNumber = strBuildNumber.\
-            arg(t.date().year()).\
-            arg(t.date().month()).\
-            arg(t.date().day()).\
-            arg(t.time().hour()).\
-            arg(t.time().minute());
-    QLabel* labelBuildTime = new QLabel(this);
-    labelBuildTime->setText(strBuildNumber);
-
-    QString strInfo = QString(tr("<span style=\"font-size:11px\">Version %2</span>")).arg(WIZ_CLIENT_VERSION);
-    QLabel* labelBuild = new QLabel(this);
-    labelBuild->setText(strInfo);
-
+    // Construct build information
+    QString versionInfo = QString(
+        tr("<span style='font-size:11px'>Version: %1</span><br/>")).arg(WIZ_CLIENT_VERSION);
     QString devInfo = QString(
-        tr("<span style=\"font-size:11px\">Development Stage %1.%2</span>").arg(WIZ_DEV_STAGE, WIZ_DEV_STAGE_VERSION));
-    QLabel* labelDev = new QLabel(this);
-    labelDev->setText(devInfo);
+        tr("<span style='font-size:11px'>Development Stage: %1.%2</span><br/>").arg(WIZ_DEV_STAGE, WIZ_DEV_STAGE_VERSION));
+    QString qtInfo = QString(
+        tr("<span style='font-size:11px'>Build against: Qt %1</span><br/>").arg(QT_VERSION_STR));
+    QString buildTime = QString(
+        tr("<span style='font-size:11px'>Build Time: %1 %2</span>")).arg(__DATE__, __TIME__);
 
-    QString qtInfo = QString(tr("<span style=\"font-size:11px\">Build against Qt %1</span>").arg(QT_VERSION_STR));
-    QLabel* labelQt = new QLabel(this);
-    labelQt->setText(qtInfo);
+    QString buildInfo = "<div style='text-align:center'>" + versionInfo + devInfo + qtInfo + buildTime +"</div>";
+    QLabel* labelBuildInfo = new QLabel(this);
+    labelBuildInfo->setText(buildInfo);
+    labelBuildInfo->setTextInteractionFlags(Qt::TextBrowserInteraction | Qt::TextSelectableByKeyboard);
 
+    // Construct credits information
     QTextBrowser* textCredits = new QTextBrowser(this);
     textCredits->setOpenExternalLinks(true);
     textCredits->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
     QString strHtml;
-
     QFile file(":/credits.html");
     if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
         QTextStream stream(&file);
         strHtml = stream.readAll();
         file.close();
     }
-
-    //Utils::Misc::loadUnicodeTextFromFile(":/credits.html", strHtml);
     textCredits->setHtml(strHtml);
 
     QLabel* labelCopyright = new QLabel(this);
-    labelCopyright->setText(tr("<span style=\"font-size:10px\">Copyright 2011-2017 Beijing Wozhi Technology Co., Ltd. All rights reserved.</span>"));
+    labelCopyright->setText(
+        tr("<span style=\"font-size:10px\">Copyright 2011-2017 Beijing Wozhi Technology Co., Ltd. All rights reserved.</span>"));
 
     QHBoxLayout *mainLayout = new QHBoxLayout(this);
     QVBoxLayout* layout = new QVBoxLayout(this);
@@ -86,17 +77,8 @@ WizAboutDialog::WizAboutDialog(QWidget *parent)
     layout->addWidget(labelProduct);
     layout->setAlignment(labelProduct, Qt::AlignCenter);
 
-    layout->addWidget(labelBuild);
-    layout->setAlignment(labelBuild, Qt::AlignCenter);
-
-    layout->addWidget(labelDev);
-    layout->setAlignment(labelDev, Qt::AlignCenter);
-
-    layout->addWidget(labelQt);
-    layout->setAlignment(labelQt, Qt::AlignCenter);
-
-    layout->addWidget(labelBuildTime);
-    layout->setAlignment(labelBuildTime, Qt::AlignCenter);
+    layout->addWidget(labelBuildInfo);
+    layout->setAlignment(labelBuildInfo, Qt::AlignCenter);
 
     layout->addSpacing(50);
     
